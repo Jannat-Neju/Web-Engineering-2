a0629cf13729eed22cad5ffa216c35e58f6f518d
Jannat-Neju <jannatneju200014@gmail.com>
2025-12-04 11:55:34 +0600

    Updated project files

--- config.php
+++ config.php
@@ -1,12 +1,20 @@
 <?php
-$host = getenv("DB_HOST") ?: 'localhost';
-$port = getenv("DB_PORT") ?: '3306';
-$dbname = getenv("DB_NAME") ?: 'shop_db';
-$user = getenv("DB_USER") ?: 'root';
-$password = getenv("DB_PASSWORD") ?: '';
 
-$conn = mysqli_connect($host, $user, $password, $dbname, $port);
+$db_host = "dpg-d4onnlnpm1nc73ec2f40-a.oregon-postgres.render.com";
+$db_port = "5432";
+$db_name = "shop_db_og56";
+$db_user = "shop_db_og56_user";
+$db_pass = "S4Uo4J8czOGMP1ut0qjkY1uWfQzKNCyx";
+
+// PostgreSQL connection (not MySQL)
+$conn = pg_connect("
+    host=$db_host 
+    port=$db_port 
+    dbname=$db_name 
+    user=$db_user 
+    password=$db_pass
+");
+
 if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+    die('Connection failed: ' . pg_last_error());
 }
-?>
